7241caaa147c4449a4b2dda2777ee820d2602fef
Renato "Lond" Cerqueira <renato@lond.com.br>
2025-10-23 14:29:10 +0200

    Use latest xvf3510 from OVOS to try to fix sound issues

--- PKGBUILDs/ovos-enclosure-audio-vocalfusion-dkms/xvf3510-init.sh
+++ PKGBUILDs/ovos-enclosure-audio-vocalfusion-dkms/xvf3510-init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Flash xmos firmware
-/opt/ovos/bin/xvf3510-flash --direct /usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_1_0.bin
+/opt/ovos/bin/xvf3510-flash --direct /usr/lib/firmware/xvf3510/app_xvf3510_int_spi_boot_v4_2_0.bin
+python /opt/ovos/bin/init_tas5806.py
 # Init TI Amp
 # sj201 init-ti-amp
